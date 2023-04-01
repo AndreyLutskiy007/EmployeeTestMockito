b952970b83fea5e17f874ac8847f5ffc847a7451
Andrey.Lutskiy <luzkiyac@mail.ru>
2023-04-01 09:19:47 +0300

    Исходный файл

---
+++ src/main/java/pro/sky/demoEmployeeTestMockito/DemoEmployeeTestMockitoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.demoEmployeeTestMockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEmployeeTestMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEmployeeTestMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/demoEmployeeTestMockito/DemoEmployeeTestMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.demoEmployeeTestMockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEmployeeTestMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
